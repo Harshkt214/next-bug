a3ff89569bb46bf4cc742180560705a650d7d232
Mediquiz <101118239+Harshkt214@users.noreply.github.com>
2024-09-02 20:33:24 +0530

    Update page.tsx

--- src/app/account/[user]/page.tsx
+++ src/app/account/[user]/page.tsx
@@ -1,11 +1,11 @@
 export const dynamic = "force-static";
 
-const getData = () => {
+const getData = async () => {
    console.log("getData ", Date.now());
    return null;
 };
-export default function User({ params }: { params: { user: string } }) {
-   const data = getData();
+export default async function User({ params }: { params: { user: string } }) {
+   const data = await getData();
    console.log(data);
    console.log(Date.now());
    return <>{JSON.stringify(params, null, 4)}</>;
